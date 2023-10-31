fc7d3a7ea2682dad9607aaf94f61adba9706e802
quanth@a2ztech.vn <quan20028a3kb@gmail.com>
2023-11-01 02:40:24 +0700

    trinhquandev: config menu

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Route, RouteProps } from "react-router-dom";
-
-// components
 import PrivateRoute from "./PrivateRoute";
 import Root from "./Root";
-
-// lazy load all the views
 
 // auth
 const Login = React.lazy(() => import("../pages/auth/Login"));
@@ -22,48 +18,28 @@ const AnalyticsDashboard = React.lazy(
 	() => import("../pages/dashboard/Analytics")
 );
 
-// apps
-// awb
-const AwbDetail = React.lazy(() => import("../pages/awb/Detail/index"));
-const AwbList = React.lazy(() => import("../pages/awb/index"));
-const AwbManager = React.lazy(() => import("../pages/awb/Manager"));
-// tracking
-const TrackingDetail = React.lazy(
-	() => import("../pages/tracking/Detail/index")
-);
-const TrackingList = React.lazy(() => import("../pages/tracking/index"));
-const TrackingManager = React.lazy(() => import("../pages/tracking/Manager"));
-const TrackingSearch = React.lazy(() => import("../pages/tracking/SearchTracking"));
-// order
-const OrderDetail = React.lazy(() => import("../pages/order/Detail/index"));
-const OrderList = React.lazy(() => import("../pages/order/index"));
-const OrderManager = React.lazy(() => import("../pages/order/Manager"));
-// customer
-const CustomerDetail = React.lazy(
-	() => import("../pages/customers/Detail/index")
-);
-const CustomerList = React.lazy(() => import("../pages/customers/index"));
-const CustomerManager = React.lazy(() => import("../pages/customers/Manager"));
-// user
-const UserDetail = React.lazy(() => import("../pages/user/Detail/index"));
-const UserList = React.lazy(() => import("../pages/user/index"));
-const UserManager = React.lazy(() => import("../pages/user/Manager"));
-// transaction
-const TransactionDetail = React.lazy(
-	() => import("../pages/transaction/Detail/index")
-);
-const TransactionList = React.lazy(() => import("../pages/transaction/index"));
-const TransactionManager = React.lazy(
-	() => import("../pages/transaction/Manager")
-);
 // setting
-const SettingDetail = React.lazy(() => import("../pages/setting/Detail/index"));
 const SettingList = React.lazy(() => import("../pages/setting/index"));
-const SettingManager = React.lazy(() => import("../pages/setting/Manager"));
 
-// extra pages
-const Error404 = React.lazy(() => import("../pages/error/Error404"));
-const Error500 = React.lazy(() => import("../pages/error/Error500"));
+// receipt
+const ReceiptList = React.lazy(() => import("../pages/receipt/index"));
+
+// delivery bill
+const DeliveryBillList = React.lazy(
+	() => import("../pages/deliveryBill/index")
+);
+
+// inventory
+const InventoryList = React.lazy(() => import("../pages/inventory/index"));
+
+// employee
+const EmployeeList = React.lazy(() => import("../pages/employee/index"));
+
+// report
+const Report = React.lazy(() => import("../pages/report/index"));
+
+// statistics
+const Statistics = React.lazy(() => import("../pages/statistics/index"));
 
 export interface RoutesProps {
 	path: RouteProps["path"];
@@ -110,175 +86,91 @@ const dashboardRoutes: RoutesProps = {
 // app
 const projectAppRoutes: RoutesProps[] = [
 	{
-		path: "/awb",
-		name: "Awb",
+		path: "/receipt",
+		name: "Receipt",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/awb/list",
-				name: "Awb List",
-				component: AwbList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/awb/details",
-				name: "Awb Detail",
-				component: AwbDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/awb/manager",
-				name: "Awb Detail",
-				component: AwbManager,
+				path: "/receipt",
+				name: "Receipt List",
+				component: ReceiptList,
 				route: PrivateRoute,
 			},
 		],
 	},
 	{
-		path: "/tracking",
-		name: "Tracking",
+		path: "/delivery-bill",
+		name: "Delivery Bill",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/tracking/search",
-				name: "Tracking Search",
-				component: TrackingSearch,
-				route: PrivateRoute,
-			},
-			{
-				path: "/tracking/list",
-				name: "Tracking List",
-				component: TrackingList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/tracking/details",
-				name: "Tracking Detail",
-				component: TrackingDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/tracking/manager",
-				name: "Tracking Detail",
-				component: TrackingManager,
+				path: "/delivery-bill",
+				name: "Delivery Bill List",
+				component: DeliveryBillList,
 				route: PrivateRoute,
 			},
 		],
 	},
 	{
-		path: "/order",
-		name: "Orer",
+		path: "/inventory",
+		name: "Inventory",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/order/list",
-				name: "Order List",
-				component: OrderList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/order/details",
-				name: "Order Detail",
-				component: OrderDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/order/manager",
-				name: "Order Detail",
-				component: OrderManager,
+				path: "/inventory",
+				name: "Inventory List",
+				component: InventoryList,
 				route: PrivateRoute,
 			},
 		],
 	},
 	{
-		path: "/customer",
-		name: "Customer",
+		path: "/employees",
+		name: "Employee",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/customer/list",
-				name: "Customer List",
-				component: CustomerList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/customer/details",
-				name: "Customer Detail",
-				component: CustomerDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/customer/manager",
-				name: "Customer Detail",
-				component: CustomerManager,
+				path: "/employees",
+				name: "Employee List",
+				component: EmployeeList,
 				route: PrivateRoute,
 			},
 		],
 	},
 	{
-		path: "/user",
-		name: "User",
+		path: "/report",
+		name: "Report",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/user/list",
-				name: "User List",
-				component: UserList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/user/details",
-				name: "User Detail",
-				component: UserDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/user/manager",
-				name: "User Detail",
-				component: UserManager,
+				path: "/report",
+				name: "Report List",
+				component: Report,
 				route: PrivateRoute,
 			},
 		],
 	},
 	{
-		path: "/transaction",
-		name: "Transaction",
+		path: "/statistics",
+		name: "Statistics",
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/transaction/list",
-				name: "Transaction List",
-				component: TransactionList,
-				route: PrivateRoute,
-			},
-			{
-				path: "/transaction/details",
-				name: "Transaction Detail",
-				component: TransactionDetail,
-				route: PrivateRoute,
-			},
-			{
-				path: "/transaction/manager",
-				name: "Transaction Detail",
-				component: TransactionManager,
+				path: "/statistics",
+				name: "Statistics List",
+				component: Statistics,
 				route: PrivateRoute,
 			},
 		],
@@ -289,24 +181,23 @@ const projectAppRoutes: RoutesProps[] = [
 		route: PrivateRoute,
 		roles: ["Admin"],
 		icon: "uil-briefcase",
-
 		children: [
 			{
-				path: "/setting/list",
-				name: "Setting List",
+				path: "/setting/role",
+				name: "Setting Role",
 				component: SettingList,
 				route: PrivateRoute,
 			},
 			{
-				path: "/setting/details",
-				name: "Setting Detail",
-				component: SettingDetail,
+				path: "/setting/product",
+				name: "Setting Product",
+				component: SettingList,
 				route: PrivateRoute,
 			},
 			{
-				path: "/setting/manager",
-				name: "Setting Detail",
-				component: SettingManager,
+				path: "/setting/supplier",
+				name: "Setting Supplier",
+				component: SettingList,
 				route: PrivateRoute,
 			},
 		],
@@ -348,22 +239,6 @@ const authRoutes: RoutesProps[] = [
 	},
 ];
 
-// public routes
-const otherPublicRoutes: RoutesProps[] = [
-	{
-		path: "/error-404",
-		name: "Error - 404",
-		component: Error404,
-		route: Route,
-	},
-	{
-		path: "/error-500",
-		name: "Error - 500",
-		component: Error500,
-		route: Route,
-	},
-];
-
 // flatten the list of all nested routes
 const flattenRoutes = (routes: RoutesProps[]) => {
 	let flatRoutes: RoutesProps[] = [];
@@ -381,7 +256,7 @@ const flattenRoutes = (routes: RoutesProps[]) => {
 
 // All routes
 const authProtectedRoutes = [rootRoute, dashboardRoutes, ...projectAppRoutes];
-const publicRoutes = [...authRoutes, ...otherPublicRoutes];
+const publicRoutes = [...authRoutes];
 
 const authProtectedFlattenRoutes = flattenRoutes([...authProtectedRoutes]);
 const publicProtectedFlattenRoutes = flattenRoutes([...publicRoutes]);
