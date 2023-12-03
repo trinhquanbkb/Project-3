5b02f3656f66b13a8c7350830f01930674bc5a67
quanth@a2ztech.vn <quan20028a3kb@gmail.com>
2023-12-04 00:35:17 +0700

    trinhquan_branch: add layout list receipt

--- backend/dist/suppliers/controllers/suppliers.controller.d.ts
+++ backend/dist/suppliers/controllers/suppliers.controller.d.ts
@@ -1,12 +1,20 @@
 import { SuppliersService } from '../services/suppliers.service';
 import { CreateSupplierDto } from '../dto/create-supplier.dto';
-import { UpdateSupplierDto } from '../dto/update-supplier.dto';
 export declare class SuppliersController {
-    private readonly suppliersService;
-    constructor(suppliersService: SuppliersService);
-    create(createSupplierDto: CreateSupplierDto): Promise<import("../schema/supplier.schema").SupplierDocument>;
-    findAll(): Promise<import("../schema/supplier.schema").SupplierDocument[]>;
-    findOne(id: string): Promise<import("../schema/supplier.schema").SupplierDocument>;
-    update(id: string, updateSupplierDto: UpdateSupplierDto): Promise<import("../schema/supplier.schema").SupplierDocument>;
-    remove(id: string): string;
+    private readonly rolesService;
+    constructor(rolesService: SuppliersService);
+    createRole(roleDto: CreateSupplierDto): Promise<import("../schema/supplier.schema").SupplierDocument>;
+    findAllRoles(pagination: any, filter: string): Promise<{
+        data: import("../schema/supplier.schema").SupplierDocument[];
+        paginations: {
+            page: any;
+            pageSize: any;
+            total: number;
+            totalPage: number;
+        };
+        messenger: string;
+    }>;
+    findRoleById(id: string): Promise<import("../schema/supplier.schema").SupplierDocument>;
+    updateRole(id: string, roleDto: CreateSupplierDto): Promise<import("../schema/supplier.schema").SupplierDocument>;
+    deleteRole(id: string): Promise<import("../schema/supplier.schema").SupplierDocument>;
 }
