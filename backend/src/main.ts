a02c687f5702ffb0fb4c710b1e5a5b5641245cb7
quanth@a2ztech.vn <quan20028a3kb@gmail.com>
2023-12-03 23:26:53 +0700

    trinhquan_branch: fix api financial_transaction

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,19 +9,18 @@ async function bootstrap() {
   app.enableCors();
 
   const userService = app.get(UsersService);
-  
-  const adminUser = await userService.findOne({email: 'admin@gmail.com'})
-  if (!adminUser) {
 
+  const adminUser = await userService.findOne({ email: 'admin@gmail.com' });
+  if (!adminUser) {
     let admin = {
-      "username": "admin",
-      "password": "123456",
-      "email": "admin@gmail.com",
-      "phone": 0,
-      "role_id": null,
-      "parent_id": null,
-      "warehouse_id": null
-    }
+      username: 'admin',
+      password: '123456',
+      email: 'admin@gmail.com',
+      phone: 0,
+      role_id: null,
+      parent_id: null,
+      warehouse_id: null,
+    };
     await userService.create(admin);
     console.log('Admin user created successfully.');
   }
@@ -40,6 +39,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
   app.use(json({ limit: '50mb' }));
-  await app.listen(3303);
+  await app.listen(3304);
 }
 bootstrap();
