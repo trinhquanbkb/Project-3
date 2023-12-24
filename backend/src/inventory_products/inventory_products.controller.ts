6ece1ccce522fbba5ed8b01eb82d1c6ac46f8772
luk6785 <buiman127@gmail.com>
2023-12-24 18:39:11 +0700

    fix thống kê theo sản phẩm tồn kho

--- backend/src/inventory_products/inventory_products.controller.ts
+++ backend/src/inventory_products/inventory_products.controller.ts
@@ -3,6 +3,7 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import * as jwt from 'jsonwebtoken';
 import { OrdersService } from 'src/product_items/services/products.service';
+import axios from 'axios';
 
 
 @Controller('InventoryProducts')
@@ -18,31 +19,49 @@ export class InventoryProductsController {
 
   @Get()
   async getStatistics(@Req() request: any): Promise<any> {
+    const apiUrl = request.protocol + '://' + request.get('host') + '/products/';
     const token = request.headers.authorization.replace('Bearer ', '');
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const warehouseId = decodedToken["warehouse_id"];
+    const warehouseId = decodedToken["warehouse_id"]._id;
+    
     if (warehouseId) {
-      const productItems: any = await this.ordersService.findRoleById(warehouseId);
+      const productItems: any = await this.ordersService.findProductItemsByWarehouseId(warehouseId);
       const productsArray: any[] = [];
-      
+      const itemsInWarehouse: any[]= [];
       for (const productId of productItems) {
-        const quantity = productId.get('quantity') || 0;
-        const quantity_sold = productId.get('quantity_sold') || 0;
-        const remainingQuantity = quantity - quantity_sold;
+        // const quantity = productId.get('quantity') || 0;
+        // const quantity_sold = productId.get('quantity_sold') || 0;
+        // const remainingQuantity = quantity - quantity_sold;
 
-        if (remainingQuantity >= 0) {
-          const product_id = productId.get('product_id')
-
-          if (product_id) {
-            const product = await this.productsService.findRoleById(product_id);
-            if (product) {
-              // const productWithDetails = { ...product.toObject(), ...productId.toObject()};
-              const productWithDetails = { ...product.toObject(), ...productId.toObject(), "remainingQuantity": remainingQuantity };
-              productsArray.push(productWithDetails);
-            }
+        const product_id = productId.get('product_id')
+        console.log(apiUrl + product_id)
+        const response = await axios.get(apiUrl + product_id, {
+          headers: {
+            Authorization: request.headers.authorization
           }
-        }
+        });
+        const data = response.data;
+
+        console.log(data)
+        productsArray.push(data);
+        
       }
+      for (const product of productsArray) {
+        const filteredProductItems = product.product_items.filter(
+          (item) => item.warehouse_id === warehouseId
+        );
+        product.product_items = filteredProductItems;
+
+      }
+
+      productsArray.forEach((product) => {
+        const totalSold = product.product_items.reduce((acc, item) => acc + item.quantity_sold, 0);
+        const inventory = product.quantity - totalSold;
+        product.total_sold = totalSold;
+        product.inventory = inventory;
+      });
+
+
       return productsArray;
     }
   }
